814d90c0a6aa986c04d18aa27768964f2cfb13c6
Abu Bakar <Abu.Bakar@abacusone.local>
2020-02-13 12:56:32 +0500

    Created cucumber.json file in the target folder

--- src/test/java/com/bdd/cicddemo/test/LoginTest.java
+++ src/test/java/com/bdd/cicddemo/test/LoginTest.java
@@ -5,6 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/login.feature")
+@CucumberOptions(
+        features = "src/test/resources/features/login.feature",
+        plugin = { "pretty", "json:target/Cucumber.json" }
+)
+//@Cucumber.Options( format={"json:target/Destination/cucumber.json"})
 public class LoginTest {
 }
